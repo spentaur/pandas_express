2e05d7b114fe367df17b4180bdf867dfed956142
Spencer Adams <spencer.adams93@gmail.com>
2019-11-04 22:05:45 -0600

    still trying to change version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pandas-express",
-    version="0.0.3",
+    version="0.0.4",
     author="spentaur",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
